e64e7248d432cf041576664fbd7eb6f680c09954
joanpaucg <joanpau.castells@hotmail.com>
2019-11-20 20:58:34 +0100

    Create Devices Done

--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/resources/DeviceResource.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/resources/DeviceResource.java
@@ -1,5 +1,61 @@
 package com.jboss.resteasy.resources;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+
+import com.jboss.resteasy.beans.Device;
+import com.jboss.resteasy.services.DeviceService;
+@Path("/devices")
 public class DeviceResource {
+	private DeviceService myDeviceService=new DeviceService();
+	@POST
+	@Path("/create_device")
+	@Consumes("multipart/form-data")
+	@Produces("application/json")
+	public Response createDevice(@MultipartForm Device form){
+		String fileName="C:\\Users\\joanpau\\Desktop\\Servidor Data\\"+form.getName()+".png";
+		try {
+			writeFile(form.getImage(), fileName);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		System.out.println("name: "+form.getName());
+		System.out.println("status: "+form.getStatus());
+		System.out.println("image: "+form.getImage());
+		System.out.println("user_id: "+form.getUserId());
+		System.out.println("Done");
+		int status=myDeviceService.createDevice(form);
+		return Response.status(Status.CREATED).build();
+		    
+	}
+	private void writeFile(byte[] content, String filename) throws IOException {
 
+		File file = new File(filename);
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileOutputStream fop = new FileOutputStream(file);
+
+		fop.write(content);
+		fop.flush();
+		fop.close();
+
+	}
 }
--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/DeviceService.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/DeviceService.java
@@ -1,5 +1,66 @@
 package com.jboss.resteasy.services;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import com.jboss.resteasy.beans.Device;
 public class DeviceService {
+	private static Statement stm;
+	private static Connection connection;
+	private static String consulta;
+	private static DataSource ds;
+	private static InitialContext ctx;
+	private static String strEstat;
+	private static ResultSet rs;
+	public int createDevice(Device device){
+		int id=0;
+		try {
+			ctx= new InitialContext();
+			if(ctx!=null) {
+				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds==null){
+					strEstat="Getting Error in Connection";
+					System.out.println(strEstat);
+					return -1; //Error Connecting The user
+				}
+				else {
+					
+					consulta="SELECT id from garduino.device order by id desc limit 1";		
+					strEstat="Connection Established";
+					connection=ds.getConnection();
+					stm= connection.createStatement();
+					rs=stm.executeQuery(consulta);
+					while(rs.next()){
+						id=(int)rs.getInt("id");
+					}
+					id=id+1;
+					System.out.println("\nsql:"+consulta);
+					System.out.println(strEstat);
+
+					consulta="insert into garduino.device (name,status,id,image,user_id) values ('"+device.getName()+"', '"+device.getStatus()+"',"+id+",'"+device.getImage()+"','"+device.getUserId()+"')";
+					System.out.println("\nsql:"+consulta);
+					stm.executeUpdate(consulta);
+
+					
+					//System.out.println("\nfora:");
+				
+					connection.close();
+					stm.close();
+					//stm.close();				
+					}
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		return id;
+	}
+	
+	
 
 }
