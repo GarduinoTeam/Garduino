3e3e4012deeddea6f7d23f34e4e5d9599c54601a
Rochii <truchero.roger@gmail.com>
2019-12-16 20:36:50 +0100

    Implemented some logic of rule time condition part, and created a thread for each device

--- RPi/master/server.py
+++ RPi/master/server.py
@@ -8,21 +8,38 @@ Created on Mon Dec  9 19:54:22 2019
 @author: Roger Truchero Visa
 """
 
+from threading import Thread
 from jsonsocket import Server
 import subprocess
 
-host = "192.168.1.131"
-port = 12346
+###############################################################################
 
-server = Server(host, port)
-
-# List of all accepted device for that raspberry pi
-accepted_devices = [ 123 ]
+host = "192.168.43.181"
+port = 12348
+    
+# List of all accepted device and operations for that raspberry pi
+accepted_devices = [ 123, 124 ]
 accepted_operations = [ "sensor", "webcam", "irrigate", "stop_irrigate" ]
+
+
+
+"""
+({ "device_id" : 123, "operation" : operation })
+
+* Sensor
+* Webcam 
+* Irrigate
+* Stop irrigate
+
+
+"""
 
 # Irrigation paths
 main_path = "house/"
 pub_path =  main_path + "response/"
+
+# List of current threads
+threads = []
 
 """
 sub_sensor = "house/sensor/{device_id}"
@@ -31,26 +48,49 @@ sub_irrigate = "house/irrigate/{device_id}"
 sub_stop_irrigate = "house/stop_irrigate/{device_id}"
 """
 
-subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + str(accepted_devices[0])])
+###############################################################################
 
-while True:
-    server.accept()
-    data = server.recv()
+# Function called by each device
+def threaded_func(server, dev):
+    subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + dev])
     
-    # If accepted device ( {device_1}, {device_2}, ...)
-    print "device_id: " + str(data["device_id"])
-    if data["device_id"] in accepted_devices:
-        device_id = data["device_id"]
-        # If accepted operation (sensor, webcam, irrigate, stop_irrigate)
-        if data["operation"] in accepted_operations: 
-            operation = data["operation"]
-            path = main_path + operation + "/" + str(device_id)
-            subprocess.call(["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + str(device_id), "-m", ""])
-            server.send({ "response" : path })        
-                
+    while True:
+        server.accept()
+        data = server.recv()
+        # If accepted device ( {device_1}, {device_2}, ...)
+        print(":threaded_func device_id: " + str(data["device_id"]))
+        if data["device_id"] in accepted_devices:
+            device_id = str(data["device_id"])
+            # If accepted operation (sensor, webcam, irrigate, stop_irrigate)
+            if data["operation"] in accepted_operations:
+                operation = data["operation"]
+                path = main_path + operation + "/" + device_id
+                subprocess.call(["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + device_id, "-m", ""])
+                server.send({ "response" : path })  
+                    
+            else:
+                server.send({ "response" : "invalid operation " +  data })
         else:
-            server.send({ "response" : "invalid operation " +  data })
-    else:
-        server.send({ "response" : "invalid device " +  data })
+            server.send({ "response" : "invalid device " +  data })
 
-server.close()
+# Main function
+def main():
+    server = Server(host, port)
+    
+    # For each accepted device create a thread to execute the "server" function 
+    for dev in accepted_devices:   
+        thread = Thread(target = threaded_func, args = (server, str(dev)))
+        thread.start()
+        threads.append(thread)
+        print(":main info: thread finished...exiting")
+    
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+        
+    print(":main info: exiting Main Thread")
+    server.close()
+    
+# Main program call
+if __name__ == "__main__":
+    main()
