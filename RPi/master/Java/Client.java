a9a328c9c65e491ae919b1370b991695e8aa700b
Rochi <truchero.roger@gmail.com>
2020-01-03 23:10:45 +0100

    Optimized Client class

--- RPi/master/Java/Client.java
+++ RPi/master/Java/Client.java
@@ -6,40 +6,82 @@ import java.io.*;
  
 public class Client 
 { 
+    private static String hostname = "192.168.43.70";
+    private static int port = 12345;
+
     public static void main(String[] args) 
     {
-        String hostname = "192.168.43.70";
-        int port = 12348;
- 
+        int argsLength = args.length;
+        int rc = 0;
+
+        if(argsLength == 2)
+        {
+            rc = sendRequest(args[0], args[1], 0);
+        }
+        else if(argsLength == 3)
+        {
+            rc = sendRequest(args[0], args[1], Integer.valueOf(args[2]));   
+        }
+        else{
+            System.exit(0);
+        }
+    }
+
+    private static int sendRequest(String operation, String device_id, int irrigation_time)
+    {
+        // Send petition
         try (Socket socket = new Socket(hostname, port)) 
         {
-            // Send JSON data
             JSONObject json = new JSONObject();
-            json.put("operation", "sensor");
-            json.put("device_id", "123");
+
+            if(operation != ""){
+                json.put("operation", operation);
+            }
+            else{
+                return -1;
+            }
+
+            if(device_id != ""){
+                json.put("device_id", device_id);
+            }
+            else{
+                return -1;
+            }
+            
+            if(irrigation_time != 0){
+                json.put("irrigation_time", irrigation_time);
+            }
+        
             String jsonData = json.toString();
 
+            // Send JSON data
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
             
+            // First send JSON data length
             System.out.println(jsonData.length());
             writer.println(String.valueOf(jsonData.length())); 
 
+            // Send the JSON
             System.out.println(jsonData);
             writer.println(String.valueOf(jsonData)); 
-        } 
-        catch (JSONException ex)
+        }
+        catch (JSONException ex) 
         {
-            System.out.println("JSON error: " + ex.getMessage());   
+            System.out.println("Invalid JSON error: " + ex.getMessage());
+            return -1;
         }
         catch (UnknownHostException ex) 
         {
             System.out.println("Server not found: " + ex.getMessage());
- 
+            return -1;
         } 
         catch (IOException ex) 
         {
             System.out.println("I/O error: " + ex.getMessage());
-        }
+            return -1;
+        }   
+
+        return 0;
     }
 }
--- RPi/master/python/server.py
+++ RPi/master/python/server.py
@@ -1,24 +1,11 @@
 import json, socket
 import subprocess
 import datetime
+import time
 from threading import Thread
 ##from jsonsocket import Server
 
 ###############################################################################
-
-HOST = '192.168.43.70'
-PORT = 12348
-
-# List of all accepted device and operations for that raspberry pi
-accepted_devices = [ 123, 124 ]
-accepted_operations = [ "sensor", "webcam", "irrigate", "stop_irrigate" ]
-
-# Irrigation paths
-main_path = "house/"
-pub_path =  main_path + "response/"
-
-# List of current threads
-threads = []
 
 """
 ({ "device_id" : 123, "operation" : operation })
@@ -36,40 +23,45 @@ sub_stop_irrigate = "house/stop_irrigate/{device_id}"
 
 ###############################################################################
 
-# Function called by each device
-def threaded_func(server):
-    #subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + dev])
+HOST = '192.168.43.70'
+PORT = 12345
+
+# List of all accepted device and operations for that raspberry pi
+accepted_devices = [ "123", "124" ]
+accepted_operations = [ "sensor", "webcam", "irrigate", "stop_irrigate" ]
+
+# Irrigation paths
+main_path = "house/"
+pub_path =  main_path + "response/"
+
+# List of current threads
+irrigate_threads = []
+
+###############################################################################
+
+# Function to control when to stop irrigation
+def threaded_func(irrigation_time, device_id):
     
-    while True:
-        conn, addr = server.accept()
-        with conn:
-            print('Connected by', addr)
+    # MQTT irrigate 
+    params = ["mosquitto_pub", "-h", "localhost", "-t", "house/irrigate" + "/" + device_id, "-m", ""]  
+    #rc = run_command(params)
 
-            data = _recv(conn)
-            # If accepted device ( {device_1}, {device_2}, ...)
-            print("{0} => RCV: data: {1}".format(datetime.datetime.now(), data))
-            if data["device_id"] in accepted_devices:
-                device_id = str(data["device_id"])
-                # If accepted operation (sensor, webcam, irrigate, stop_irrigate)
-                if data["operation"] in accepted_operations:
-                    operation = data["operation"]
-                    path = main_path + operation + "/" + device_id
-                    """params = ["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + device_id, "-m", ""]
-                    rc = run_command(params)
-                    #output = subprocess.Popen(params, stdout = subprocess.PIPE ).communicate()[0]
-                    print("{0} => SND: output: {1}".format(datetime.datetime.now(), rc))
-                    conn.send({ "response" : rc })
-                    """
-                    print("{0} => SND: output: {1}".format(datetime.datetime.now(), "asdf"))
-                    #conn.send({ "response" : "Aqui aniria la resposta" })
-                else:
-                    continue
-                    #conn.send({ "response" : "invalid operation " +  data })
-            else:
-                continue
-                #conn.send({ "response" : "invalid device " +  data })
+    start_time = time.time()
+    end_time = start_time + irrigation_time * 60
 
-#
+    print("{0} => start_time: {1} end_time: {2} device_id: {3} info: starting irrigation".format(datetime.datetime.now(), start_time, end_time, device_id))
+
+    while(end_time > time.time()):
+        time.sleep(5)
+    
+    # MQTT stop_irrigate
+    params = ["mosquitto_pub", "-h", "localhost", "-t", "house/stop_irrigate" + "/" + device_id, "-m", ""]  
+    #rc = run_command(params)                      
+
+    print("{0} => start_time: {1} end_time: {2} device_id: {3} info: stopping irrigation".format(datetime.datetime.now(), start_time, end_time, device_id))
+    return 0
+
+# Custom recv function
 def _recv(conn):
     # read the length of the data, letter by letter until we reach EOL    
     length_str = conn.recv(1024)
@@ -89,25 +81,27 @@ def _recv(conn):
     
     return deserialized
 
-#
+# Function to read the MQTT output
 def run_command(params):
-    process = subprocess.Popen(params, stdout=subprocess.PIPE)
+    process = subprocess.Popen(params, stdout = subprocess.PIPE)
     while True:
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
             break
-        if output:
+        """if output:
             print("output: " + output.strip())
+        """
     rc = process.poll()
-    print("Return code: " + str(rc))
-    return rc
+    #print("Return code: " + str(rc))
 
+    return rc
 
 # Main function
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((HOST, PORT))
         server.listen()
+
         for dev in accepted_devices:
             break
             #subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + dev])
@@ -115,22 +109,30 @@ def main():
         while True:
             conn, addr = server.accept()
             with conn:
-                print('Connected by', addr)
+                print("{0} => Connected by: {1}".format(datetime.datetime.now(), addr))                
                 data = _recv(conn)
                 # If accepted device ( {device_1}, {device_2}, ...)
                 print("{0} => RCV: data: {1}".format(datetime.datetime.now(), data))
+
                 if data["device_id"] in accepted_devices:
                     device_id = str(data["device_id"])
                     # If accepted operation (sensor, webcam, irrigate, stop_irrigate)
                     if data["operation"] in accepted_operations:
-                        operation = data["operation"]
+                        operation = data["operation"]                    
                         path = main_path + operation + "/" + device_id
-                        """params = ["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + device_id, "-m", ""]
-                        rc = run_command(params)
+
+                        if operation == "irrigate":
+                            irrigation_time = int(data["irrigation_time"])
+                            thread = Thread(target = threaded_func, args = (irrigation_time, dev))
+                            thread.start()
+                            irrigate_threads.append(thread)
+
+                        path = main_path + operation + "/" + device_id
+                        params = ["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + device_id, "-m", ""]
+                        #rc = run_command(params)
                         #output = subprocess.Popen(params, stdout = subprocess.PIPE ).communicate()[0]
-                        print("{0} => SND: output: {1}".format(datetime.datetime.now(), rc))
-                        conn.send({ "response" : rc })
-                        """
+                        #print("{0} => SND: output: {1}".format(datetime.datetime.now(), rc))
+                        #conn.send({ "response" : rc })                        
                         print("{0} => SND: output: {1}".format(datetime.datetime.now(), "asdf"))
                         #conn.send({ "response" : "Aqui aniria la resposta" })
                     else:
@@ -139,20 +141,10 @@ def main():
                 else:
                     continue
                     #conn.send({ "response" : "invalid device " +  data })
-"""
-        # For each accepted device create a thread to execute the "server" function 
-        for dev in accepted_devices:   
-            thread = Thread(target = threaded_func, args = (s, str(dev)))
-            thread.start()
-            threads.append(thread)
-            print(":main info: thread finished...exiting")
 
         # Wait for all threads to complete
-        for t in threads:
+        for t in irrigate_threads:
             t.join()
-        
-        print(":main info: exiting Main Thread")    
-"""
 
 # Main program call
 if __name__ == "__main__":
