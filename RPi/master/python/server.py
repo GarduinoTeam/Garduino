571127621614475fe179bd1cb27edbba51bfff00
Rochi <truchero.roger@gmail.com>
2020-01-10 20:52:15 +0100

    Repaired _send bug

--- RPi/master/python/server.py
+++ RPi/master/python/server.py
@@ -7,22 +7,6 @@ from threading import Thread
 
 ###############################################################################
 
-"""
-({ "device_id" : 123, "operation" : operation })
-
-* Sensor
-* Webcam 
-* Irrigate
-* Stop irrigate
-
-sub_sensor = "house/sensor/{device_id}"
-sub_webcam = "house/webcam/{device_id}"
-sub_irrigate = "house/irrigate/{device_id}"
-sub_stop_irrigate = "house/stop_irrigate/{device_id}"
-"""
-
-###############################################################################
-
 # List of all accepted device and operations for that raspberry pi
 accepted_devices = [ "123", "124" ]
 accepted_operations = [ "sensor", "webcam", "irrigate", "stop_irrigate" ]
@@ -60,7 +44,7 @@ def threaded_func(irrigation_time, device_id):
     
     # MQTT irrigate 
     params = ["mosquitto_pub", "-h", "localhost", "-t", "house/irrigate" + "/" + device_id, "-m", ""]  
-    #rc = run_command(params)
+    rc = run_command(params)
 
     start_time = time.time()
     end_time = start_time + irrigation_time * 60
@@ -72,7 +56,7 @@ def threaded_func(irrigation_time, device_id):
     
     # MQTT stop_irrigate
     params = ["mosquitto_pub", "-h", "localhost", "-t", "house/stop_irrigate" + "/" + device_id, "-m", ""]  
-    #rc = run_command(params)                      
+    rc = run_command(params)                      
 
     print("{0} => start_time: {1} end_time: {2} device_id: {3} info: stopping irrigation".format(datetime.datetime.now(), start_time, end_time, device_id))
     return 0
@@ -81,9 +65,8 @@ def threaded_func(irrigation_time, device_id):
 def _recv(conn):
     # read the length of the data, letter by letter until we reach EOL    
     length_str = conn.recv(1024)
-    print("length_str: {0}".format(length_str))
     total = int(length_str)
-    print("Length: {0}".format(total))
+    #print("Length: {0}".format(total))
 
     # use a memoryview to receive the data chunk by chunk efficiently
     view = memoryview(bytearray(total))
@@ -98,19 +81,27 @@ def _recv(conn):
     
     return deserialized
 
+# Custom send function
+def _send(conn, data):
+    try:
+        serialized = json.dumps(data) + "\n"
+    except (TypeError, ValueError):
+        raise Exception("You can only read JSON-serializable data")
+
+    conn.send(serialized.encode('utf-8'))
+
 # Function to read the MQTT output
 def run_command(params):
     process = subprocess.Popen(params, stdout = subprocess.PIPE)
-    while True:
+    """while True:
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
             break
-        """if output:
-            print("output: " + output.strip())
-        """
+            if output:
+                print("output: " + output.strip())        
     rc = process.poll()
-    #print("Return code: " + str(rc))
-    return rc
+    #print("Return code: " + str(rc))"""
+    return 0
 
 
 # Function that runs the main server code
@@ -120,14 +111,14 @@ def run_server(HOST, PORT):
         server.listen()
 
         for dev in accepted_devices:
-            break
-            #subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + dev])
+            subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "house/response/" + dev])
             
         while True:
             conn, addr = server.accept()
             with conn:
                 print("{0} => Connected by: {1}".format(datetime.datetime.now(), addr))                
                 data = _recv(conn)
+
                 # If accepted device ( {device_1}, {device_2}, ...)
                 print("{0} => RCV: data: {1}".format(datetime.datetime.now(), data))
 
@@ -144,11 +135,12 @@ def run_server(HOST, PORT):
                     elif (operation == "stop_irrigate" or operation == "webcam" or operation == "sensor") and device_id in accepted_devices:
                         path = main_path + operation + "/" + device_id
                         params = ["mosquitto_pub", "-h", "localhost", "-t", "house/" + operation + "/" + device_id, "-m", ""]
-                        #rc = run_command(params)
+                        
+                        rc = run_command(params)
                         #output = subprocess.Popen(params, stdout = subprocess.PIPE ).communicate()[0]
-                        #print("{0} => SND: output: {1}".format(datetime.datetime.now(), rc))
-                        #conn.send({ "response" : rc })                        
-                        print("{0} => SND: output: {1}".format(datetime.datetime.now(), "asdf"))
+                        
+                        print("{0} => SND: output: {1}".format(datetime.datetime.now(), rc))
+                        _send(conn, { "response" : rc })
 
                     elif operation == "create_device":
                         accepted_devices.append(device_id)
