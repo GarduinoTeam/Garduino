e85ce0ff9fe7c66dd73589713f4cd6716e8d82b1
Rochii <truchero.roger@gmail.com>
2020-05-07 22:11:54 +0200

    ConditionResource class clean

--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/resources/ConditionResource.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/resources/ConditionResource.java
@@ -1,8 +1,6 @@
 package com.jboss.resteasy.resources;
 import java.util.HashMap;
 import java.util.List;
-
-//import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,29 +9,35 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
 import com.jboss.resteasy.beans.Condition;
 import com.jboss.resteasy.services.ConditionService;
 
+
 @Path("/conditions")
-public class ConditionResource {
-	private ConditionService conditionService=new ConditionService();
+public class ConditionResource 
+{
+	private ConditionService conditionService = new ConditionService();
+	
+	
 	@POST
 	@Path("/create_condition")
 	@Produces("application/json")
 	@Consumes("application/json")
-	public Response createCondition(Condition condition){
-		int status=conditionService.createCondition(condition);
-		if(status==-1){
+	public Response createCondition(Condition condition)
+	{
+		int status = conditionService.createCondition(condition);
+		if(status == -1){
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 			
-		}else if(status==-2){
+		}
+		else if(status == -2){
 			return Response.status(Status.FORBIDDEN).build();
 		
-		}else{
+		}
+		else
+		{
 			condition.setId(status);
 			return Response
 					.status(Status.CREATED)
@@ -41,42 +45,54 @@ public class ConditionResource {
 					.build();
 		}
 	}
+	
+	
 	@DELETE
 	@Produces("application/json")
 	@Consumes("application/json")
 	@Path("/delete_condition/{id}")
-	public Response deleteCondition(@PathParam("id") int id){
-		int status=conditionService.deleteCondition(id);
-		if(status==-1){
+	public Response deleteCondition(@PathParam("id") int id)
+	{
+		int status = conditionService.deleteCondition(id);
+		if(status == -1){
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		}else if(status==-2){
+		}
+		else if(status == -2){
 			return Response.status(Status.NO_CONTENT).build();
-		}else{
+		}
+		else{
 			return Response.status(Status.OK).build();
 		}
-		
 	}
+	
+	
 	@PUT
 	@Produces("application/json")
 	@Consumes("application/json")
 	@Path("/update_condition/{id}")
-	public Response updateCondition(@PathParam("id") int id,Condition condition){
-		int status=conditionService.updateCondition(id,condition);
-		if(status==-1){
+	public Response updateCondition(@PathParam("id") int id,Condition condition)
+	{
+		int status = conditionService.updateCondition(id,condition);
+		if(status == -1){
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		}else if(status==-2){
+		}
+		else if(status == -2){
 			return Response.status(Status.NOT_FOUND).build();
-		}else{
+		}
+		else{
 			return Response.status(Status.OK).build();
 		}
 	}
+	
+	
 	@GET
 	@Produces("application/json")
 	@Consumes("application/json")
 	@Path("/get_conditions")
-	public Response getConditions(){
-		HashMap<String,List<Condition>> conditionsMap=new HashMap<>();
-		List<Condition> conditions=conditionService.getConditions(); 
+	public Response getConditions()
+	{
+		HashMap<String, List<Condition>> conditionsMap = new HashMap<>();
+		List<Condition> conditions = conditionService.getConditions(); 
 		conditionsMap.put("conditions", conditions);
 		return Response
 				.status(Status.OK)
@@ -84,23 +100,22 @@ public class ConditionResource {
 				.build();
 	}
 	
+	
 	@GET
 	@Produces("application/json")
 	@Consumes("application/json")
 	@Path("get_condition/{id}")
-	public Response getCondition(@PathParam("id") int id){
-		Condition condition=conditionService.getCondition(id);
-		if(condition==null){
+	public Response getCondition(@PathParam("id") int id)
+	{
+		Condition condition = conditionService.getCondition(id);
+		if(condition == null){
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		}else{
+		}
+		else{
 			return Response
 					.status(Status.OK)
 					.entity(condition)
 					.build();
-
 		}
-		
 	}
-	
-
 }
