3a4e0c45cb82706a1b14b8b851963f34ff8acfca
Rochii <truchero.roger@gmail.com>
2020-05-07 22:00:32 +0200

    SensorTypeService class clean

--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/SensorTypeService.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/SensorTypeService.java
@@ -3,13 +3,13 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-
-import com.jboss.resteasy.beans.Condition;
 import com.jboss.resteasy.beans.SensorType;
-public class SensorTypeService {
+
+
+public class SensorTypeService 
+{
 	private static Statement stm;
 	private static Connection connection;
 	private static String consulta;
@@ -17,78 +17,90 @@ public class SensorTypeService {
 	private static InitialContext ctx;
 	private static String strEstat;
 	private static ResultSet rs;
-	public int createSensorType(SensorType sensorType){
-		int id=0;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int createSensorType(SensorType sensorType)
+	{
+		int id = 0;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existSensorType=0;
-					consulta="SELECT COUNT(id) from garduino.sensor_type where name='"+sensorType.getName()+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existSensorType = 0;
+					consulta = "SELECT COUNT(id) from garduino.sensor_type where name='" + sensorType.getName() + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						existSensorType=(int)rs.getLong("count");
+						existSensorType = (int)rs.getLong("count");
 					}
-					if(existSensorType!=0){
+					
+					if(existSensorType != 0){
 						return -2;
 					}
-					consulta="SELECT id from garduino.sensor_type order by id desc limit 1";		
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+					
+					consulta = "SELECT id from garduino.sensor_type order by id desc limit 1";		
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						id=(int)rs.getInt("id");
+						id = (int)rs.getInt("id");
 					}
-					id=id+1;
+					
+					id += 1;
 					System.out.println("\nsql:"+consulta);
 					System.out.println(strEstat);
 
-					consulta="insert into garduino.sensor_type (id,name) values ('"+id+"', '"+sensorType.getName()+"')";
+					consulta = "insert into garduino.sensor_type (id,name) values ('" + id + "', '" + sensorType.getName()+"')";
 					stm.executeUpdate(consulta);
 
-					
-					//System.out.println("\nfora:");
-				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
+				}
 			}
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-			
 		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return id;
 	}
-	public int deleteSensorType(int id){
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int deleteSensorType(int id)
+	{
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existSensorType=0;
-					consulta="SELECT COUNT(id) from garduino.sensor_type where id='"+id+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existSensorType = 0;
+					consulta = "SELECT COUNT(id) from garduino.sensor_type where id='" + id + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
 						existSensorType=(int)rs.getLong("count");
 					}
@@ -129,119 +141,120 @@ public class SensorTypeService {
 					stm= connection.createStatement();
 					rs=stm.executeQuery(consulta);
 					while(rs.next()){
-						existSensorType=(int)rs.getLong("count");
+						existSensorType = (int)rs.getLong("count");
 					}
-					if(existSensorType==0){
+					
+					if(existSensorType == 0){
 						return -2;
 					}
 
-					consulta="update garduino.sensor_type set name = '" +sensortype.getName()+ "' where id='"+id+"'";
+					consulta = "update garduino.sensor_type set name = '" + sensortype.getName() + "' where id='" + id + "'";
 					System.out.println("\n update:"+consulta);
 					stm.executeUpdate(consulta);
-
-					
-					//System.out.println("\nfora:");
-				
+		
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
+				}				
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return -1;
 		}
+		
 		return 0;
-
 	}
-	public ArrayList<SensorType> getSensorTypes(){
-		ArrayList<SensorType> sensorTypes=new ArrayList<>();
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public ArrayList<SensorType> getSensorTypes()
+	{
+		ArrayList<SensorType> sensorTypes = new ArrayList<>();
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return sensorTypes; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.sensor_type"; 
+				else 
+				{
+					consulta = "select * from garduino.sensor_type"; 
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
-					while(rs.next()){
-						String name=rs.getString("name");
-						int id=rs.getInt("id");
-						SensorType sensorType=new SensorType();
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
+					while(rs.next())
+					{
+						String name = rs.getString("name");
+						int id = rs.getInt("id");
+						SensorType sensorType = new SensorType();
 						sensorType.setId(id);
 						sensorType.setName(name);
 						sensorTypes.add(sensorType);
 					}
 
-					
-					//System.out.println("\nfora:");
-				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
+				}				
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return sensorTypes;
 		}
+		
 		return sensorTypes;
-
 	}
-	public SensorType getSensorType(int id){
-		SensorType sensorType=null;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public SensorType getSensorType(int id)
+	{
+		SensorType sensorType = null;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return null; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.sensor_type where id ='"+id+"'"; 
+				else 
+				{
+					consulta = "select * from garduino.sensor_type where id ='" + id + "'"; 
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
-					while(rs.next()){
-						String name=rs.getString("name");
-						sensorType=new SensorType();
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
+					while(rs.next())
+					{
+						String name = rs.getString("name");
+						sensorType = new SensorType();
 						sensorType.setId(id);
 						sensorType.setName(name);
 					}
-
-					
-					//System.out.println("\nfora:");
 				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
+				}				
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return null;
 		}
 
-		
 		return sensorType;
 	}
-
 }
