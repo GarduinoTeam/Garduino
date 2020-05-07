137bdd1eaa087ccecadb8fa47df8b205249e81c2
Rochii <truchero.roger@gmail.com>
2020-05-07 21:42:21 +0200

    RuleConditionService class clean

--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/RuleConditionService.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/RuleConditionService.java
@@ -5,10 +5,11 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-
-import com.jboss.resteasy.beans.Rule;
 import com.jboss.resteasy.beans.RuleCondition;
-public class RuleConditionService {
+
+
+public class RuleConditionService 
+{
 	private static Statement stm;
 	private static Connection connection;
 	private static String consulta;
@@ -16,163 +17,178 @@ public class RuleConditionService {
 	private static InitialContext ctx;
 	private static String strEstat;
 	private static ResultSet rs;
-	public int createRuleCondition(RuleCondition rulecondition){
-		int id=0;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+
+	
+	public int createRuleCondition(RuleCondition rulecondition)
+	{
+		int id = 0;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-			
-					consulta="SELECT id from garduino.rule_condition order by id desc limit 1";		
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					consulta = "SELECT id from garduino.rule_condition order by id desc limit 1";		
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						id=(int)rs.getInt("id");
+						id = (int)rs.getInt("id");
 					}
-					id=id+1;
+					id += 1;
 					System.out.println("\nsql:"+consulta);
 					System.out.println(strEstat);
 
-					consulta="insert into garduino.rule_condition (id,id_rule,id_condition,condition_value,status) values ('"+id+"', '"+rulecondition.getIdRule()+"',"+rulecondition.getIdCondition()+",'"+rulecondition.getConditionValue()+"','"+rulecondition.getStatus()+"')";
+					consulta = "insert into garduino.rule_condition (id,id_rule,id_condition,condition_value,status) values ('" + id + "', '" + rulecondition.getIdRule() + "'," + rulecondition.getIdCondition() + ",'" + rulecondition.getConditionValue()+"','"+rulecondition.getStatus()+"')";
 					stm.executeUpdate(consulta);
 					
-					//System.out.println("\nfora:");
-				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();			
+				}
 			}
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-			
 		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return id;
 	}
-	public int deleteRuleCondition(int id){
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int deleteRuleCondition(int id)
+	{
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) {
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existRuleCondition=0;
-					consulta="SELECT COUNT(id) from garduino.rule_condition where id='"+id+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existRuleCondition = 0;
+					consulta = "SELECT COUNT(id) from garduino.rule_condition where id='" + id + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						existRuleCondition=(int)rs.getLong("count");
+						existRuleCondition = (int)rs.getLong("count");
 					}
-					if(existRuleCondition==0){
+					if(existRuleCondition == 0){
 						return -2;
 					}
 
-					consulta="delete from garduino.rule_condition where id='"+id+"'";
+					consulta = "delete from garduino.rule_condition where id='" + id + "'";
 					stm.executeUpdate(consulta);
-					
-					//System.out.println("\nfora:");
 				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();				
+				}
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return -1;
 		}
-		return 0;
-		
+	
+		return 0;		
 	}
-	public int updateRuleCondition(int id,RuleCondition rulecondition){
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int updateRuleCondition(int id,RuleCondition rulecondition)
+	{
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existRuleCondition=0;
-					consulta="SELECT COUNT(id) from garduino.rule_condition " +"where id='"+id+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existRuleCondition = 0;
+					consulta = "SELECT COUNT(id) from garduino.rule_condition where id='" + id + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						existRuleCondition=(int)rs.getLong("count");
+						existRuleCondition = (int)rs.getLong("count");
 					}
-					if(existRuleCondition==0){
+					if(existRuleCondition == 0){
 						return -2;
 					}
 
-					consulta="update garduino.rule_condition set status = '" +rulecondition.getStatus()+"', condition_value= '"+rulecondition.getConditionValue()+"'" +" where id='"+id+"'";
+					consulta = "update garduino.rule_condition set status = '" +rulecondition.getStatus()+"', condition_value= '"+rulecondition.getConditionValue()+"'" +" where id='"+id+"'";
 					System.out.println("\n update:"+consulta);
 					stm.executeUpdate(consulta);
 
-					
-					//System.out.println("\nfora:");
-				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
-			}
-			
-		}catch(Exception e) {
+				}				
+			}			
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return -1;
 		}
-		return 0;
 		
+		return 0;
 	}
-	public ArrayList<RuleCondition> getRuleConditions(int rule_id){
-		ArrayList<RuleCondition> ruleconditions=new ArrayList<>();
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public ArrayList<RuleCondition> getRuleConditions(int rule_id)
+	{
+		ArrayList<RuleCondition> ruleconditions = new ArrayList<>();
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return ruleconditions; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.rule_condition where id_rule='"+rule_id+"'";
+				else 
+				{	
+					consulta = "select * from garduino.rule_condition where id_rule='" + rule_id + "'";
 					
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
-					while(rs.next()){
-						int id=rs.getInt("id");
-						int id_rule=rs.getInt("id_rule");
-						int id_condition=rs.getInt("id_condition");
-						int conditionValue=rs.getInt("condition_value");
-						boolean status=rs.getBoolean("status");
-						RuleCondition rulecondition=new RuleCondition();
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
+					while(rs.next())
+					{
+						int id = rs.getInt("id");
+						int id_rule = rs.getInt("id_rule");
+						int id_condition = rs.getInt("id_condition");
+						int conditionValue = rs.getInt("condition_value");
+						boolean status = rs.getBoolean("status");
+						RuleCondition rulecondition = new RuleCondition();
 						rulecondition.setId(id);
 						rulecondition.setIdRule(id_rule);
 						rulecondition.setIdCondition(id_condition);
@@ -180,48 +196,51 @@ public class RuleConditionService {
 						rulecondition.setStatus(status);
 						ruleconditions.add(rulecondition);
 					}
-
 					
-					//System.out.println("\nfora:");
-				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
-			}
-			
-		}catch(Exception e) {
+				}
+			}			
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return ruleconditions;
 		}
-		return ruleconditions;
 		
+		return ruleconditions;
 	}
-	public RuleCondition getRuleCondition(int id){
-		RuleCondition rulecondition=null;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+
+	
+	public RuleCondition getRuleCondition(int id)
+	{
+		RuleCondition rulecondition = null;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return null; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.rule_condition where id ='"+id+"'"; 
+				else 
+				{
+					consulta = "select * from garduino.rule_condition where id ='" + id + "'"; 
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
-					while(rs.next()){
-						int id_rule=rs.getInt("id_rule");
-						int id_condition=rs.getInt("id_condition");
-						int conditionValue=rs.getInt("condition_value");
-						boolean status=rs.getBoolean("status");
-						rulecondition=new RuleCondition();
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
+					while(rs.next())
+					{
+						int id_rule = rs.getInt("id_rule");
+						int id_condition = rs.getInt("id_condition");
+						int conditionValue = rs.getInt("condition_value");
+						boolean status = rs.getBoolean("status");
+						rulecondition = new RuleCondition();
 						rulecondition.setId(id);
 						rulecondition.setIdRule(id_rule);
 						rulecondition.setIdCondition(id_condition);
@@ -229,22 +248,17 @@ public class RuleConditionService {
 						rulecondition.setStatus(status);
 					}
 
-					
-					//System.out.println("\nfora:");
-				
 					connection.close();
 					stm.close();
-					//stm.close();				
-					}
-				
+				}				
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return null;
 		}
 
-		
 		return rulecondition;
 	}
 }
