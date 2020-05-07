ae92d008a375eb321bf867f90cdb24a1e627e626
Rochii <truchero.roger@gmail.com>
2020-05-07 21:34:58 +0200

    ConditionService class clean

--- WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/ConditionService.java
+++ WebService/BackEnd/GarduinoApi/src/com/jboss/resteasy/services/ConditionService.java
@@ -9,7 +9,9 @@ import javax.sql.DataSource;
 
 import com.jboss.resteasy.beans.Condition;
 
-public class ConditionService {
+
+public class ConditionService 
+{
 	private static Statement stm;
 	private static Connection connection;
 	private static String consulta;
@@ -17,239 +19,256 @@ public class ConditionService {
 	private static InitialContext ctx;
 	private static String strEstat;
 	private static ResultSet rs;
-	public int createCondition(Condition condition){
-		int id=0;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+
+	
+	public int createCondition(Condition condition)
+	{
+		int id = 0;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existCondition=0;
-					consulta="SELECT COUNT(id) from garduino.condition where name='"+condition.getName()+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existCondition = 0;
+					consulta = "SELECT COUNT(id) from garduino.condition where name='" + condition.getName() + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
 						existCondition=(int)rs.getLong("count");
 					}
-					if(existCondition!=0){
+					
+					if(existCondition != 0){
 						return -2;
 					}
-					consulta="SELECT id from garduino.condition order by id desc limit 1";		
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+					
+					consulta = "SELECT id from garduino.condition order by id desc limit 1";		
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						id=(int)rs.getInt("id");
+						id = (int)rs.getInt("id");
 					}
-					id=id+1;
-					System.out.println("\nsql:"+consulta);
+					id += 1;
+					System.out.println("\nsql:"  +consulta);
 					System.out.println(strEstat);
 
-					consulta="insert into garduino.condition (id,name,measure) values ('"+id+"', '"+condition.getName()+"',"+"'"+condition.getMeasure()+"')";
+					consulta = "insert into garduino.condition (id,name,measure) values ('" + id + "', '" + condition.getName() + "'," + "'" + condition.getMeasure() + "')";
 					System.out.println(consulta);
 					stm.executeUpdate(consulta);
-
-					
-					//System.out.println("\nfora:");
 				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();				
+				}
 			}
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-			
 		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return id;
-		
 	}
-	public int deleteCondition(int id){
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int deleteCondition(int id)
+	{
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existCondition=0;
-					consulta="SELECT COUNT(id) from garduino.condition where id='"+id+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existCondition = 0;
+					consulta = "SELECT COUNT(id) from garduino.condition where id='" + id + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						existCondition=(int)rs.getLong("count");
+						existCondition = (int)rs.getLong("count");
 					}
-					if(existCondition==0){
+					
+					if(existCondition == 0){
 						return -2;
 					}
 
-					consulta="delete from garduino.condition where id='"+id+"'";
+					consulta = "delete from garduino.condition where id='" + id + "'";
 					stm.executeUpdate(consulta);
-
-					
-					//System.out.println("\nfora:");
 				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();				
+				}
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return -1;
 		}
-		return 0;
 		
+		return 0;
 	}
-	public int updateCondition(int id,Condition condition){
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public int updateCondition(int id,Condition condition)
+	{
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return -1; //Error Connecting The user
 				}
-				else {
-					int existCondition=0;
-					consulta="SELECT COUNT(id) from garduino.condition " +"where id='"+id+"'";
-					strEstat="Connection Established";
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+				else 
+				{
+					int existCondition = 0;
+					consulta = "SELECT COUNT(id) from garduino.condition where id='" + id + "'";
+					strEstat = "Connection Established";
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						existCondition=(int)rs.getLong("count");
+						existCondition = (int)rs.getLong("count");
 					}
-					if(existCondition==0){
+					
+					if(existCondition == 0){
 						return -2;
 					}
 
-					consulta="update garduino.condition set name = '" +condition.getName()+"',measure='"+condition.getMeasure()+"'"+ " where id='"+id+"'";
-					System.out.println("\n update:"+consulta);
+					consulta = "update garduino.condition set name = '" + condition.getName() + "',measure='" + condition.getMeasure() + "'" + " where id='" + id + "'";
+					System.out.println("\n update:" + consulta);
 					stm.executeUpdate(consulta);
-
-					
-					//System.out.println("\nfora:");
 				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();				
+				}
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return -1;
 		}
+	
 		return 0;
 	}
-	public ArrayList<Condition> getConditions(){
+	
+	
+	public ArrayList<Condition> getConditions()
+	{
 		ArrayList<Condition> conditions=new ArrayList<>();
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return conditions; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.condition"; 
+				else 
+				{
+					consulta = "select * from garduino.condition"; 
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
 					while(rs.next()){
-						String name=rs.getString("name");
-						int id=rs.getInt("id");
-						String measure=rs.getString("measure");
-						Condition condition=new Condition();
+						String name = rs.getString("name");
+						int id = rs.getInt("id");
+						String measure = rs.getString("measure");
+						Condition condition = new Condition();
 						condition.setId(id);
 						condition.setName(name);
 						condition.setMeasure(measure);
 						conditions.add(condition);
 					}
 
-					
-					//System.out.println("\nfora:");
-				
 					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
+					stm.close();				
+				}
 			}
-			
-		}catch(Exception e) {
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return conditions;
 		}
+		
 		return conditions;
 	}
-	public Condition getCondition(int id){
-		Condition condition=null;
-		try {
-			ctx= new InitialContext();
-			if(ctx!=null) {
-				ds=(DataSource) ctx.lookup("java:jboss/PostgresXA");
-				if(ds==null){
-					strEstat="Getting Error in Connection";
+	
+	
+	public Condition getCondition(int id)
+	{
+		Condition condition = null;
+		try 
+		{
+			ctx = new InitialContext();
+			if(ctx != null) 
+			{
+				ds = (DataSource) ctx.lookup("java:jboss/PostgresXA");
+				if(ds == null)
+				{
+					strEstat = "Getting Error in Connection";
 					System.out.println(strEstat);
 					return null; //Error Connecting The user
 				}
-				else {
-					
-					consulta="select * from garduino.condition where id ='"+id+"'"; 
+				else 
+				{	
+					consulta = "select * from garduino.condition where id ='" + id + "'"; 
 					System.out.println(consulta);
-					connection=ds.getConnection();
-					stm= connection.createStatement();
-					rs=stm.executeQuery(consulta);
-					while(rs.next()){
-						String name=rs.getString("name");
-						String measure=rs.getString("measure");
-						condition=new Condition();
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+					rs = stm.executeQuery(consulta);
+					while(rs.next())
+					{
+						String name = rs.getString("name");
+						String measure = rs.getString("measure");
+						condition = new Condition();
 						condition.setId(id);
 						condition.setName(name);
 						condition.setMeasure(measure);
 					}
-
-					
-					//System.out.println("\nfora:");
-				
-					connection.close();
-					stm.close();
-					//stm.close();				
-					}
-				
-			}
 			
-		}catch(Exception e) {
+					connection.close();
+					stm.close();				
+				}
+			}
+		}
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			return null;
 		}
 
-		
 		return condition;
 	}
 }
